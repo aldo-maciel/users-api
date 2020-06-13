f5f332ebd79d56c770714f840f92bccad8536ac8
Aldo Bernardes <aldo-maciel@hotmail.com>
2020-06-13 01:53:43 -0300

    feat: project generation app

---
+++ ui-view/src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { UsersView } from './users/users';
+
+function App() {
+    return (
+        <div className="app">
+            <UsersView />
+        </div>
+    );
+}
+
+export default App;
